5d58628492cec1a979692ff80c72cde40af12e7d
ketan.vichare <ketan.vichare@snapdeal.com>
2016-10-09 23:05:43 +0530

    Excel Util methods Added.

--- app/src/main/java/com/keto/optics/activity/DashboardActivity.java
+++ app/src/main/java/com/keto/optics/activity/DashboardActivity.java
@@ -1,10 +1,16 @@
 package com.keto.optics.activity;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -24,12 +30,34 @@ import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.keto.optics.R;
 import com.keto.optics.utils.DownloadImageTask;
+import com.keto.optics.utils.ExcelUtils;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+
+import jxl.Workbook;
+import jxl.WorkbookSettings;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
+
+
+@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
 public class DashboardActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     TextView userNameTextView, userEmailIdTextView;
     ImageView userProfileImageView;
+    GoogleSignInOptions mSignInOptions;
+    GoogleApiClient mGoogleApiClient;
+
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +88,8 @@ public class DashboardActivity extends AppCompatActivity
 
         initViews(navHeaderView);
         getData();
+
+        verifyStoragePermissions(this);
     }
 
     private void initViews(View navigationHeaderView){
@@ -68,6 +98,20 @@ public class DashboardActivity extends AppCompatActivity
         userNameTextView = (TextView) navigationHeaderView.findViewById(R.id.user_full_name_text_view);
         userEmailIdTextView  = (TextView) navigationHeaderView.findViewById(R.id.user_email_id_text_view);
         userProfileImageView = (ImageView) navigationHeaderView.findViewById(R.id.user_profile_imageView);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mSignInOptions = new GoogleSignInOptions.Builder()
+                .requestIdToken(getString(R.string.client_id))
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, mSignInOptions)
+                .build();
+
+        mGoogleApiClient.connect();
     }
 
     private void getData(){
@@ -129,7 +173,8 @@ public class DashboardActivity extends AppCompatActivity
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_generate_excel) {
+            generateExcelFile();
 
         } else if (id == R.id.nav_manage) {
 
@@ -147,17 +192,6 @@ public class DashboardActivity extends AppCompatActivity
     }
 
     private void signOut(){
-
-        GoogleSignInOptions mSignInOptions = new GoogleSignInOptions.Builder()
-                .requestIdToken(getString(R.string.client_id))
-                .build();
-
-        GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, mSignInOptions)
-                .build();
-
-        mGoogleApiClient.connect();
-
         Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>() {
             @Override
             public void onResult(@NonNull Status status) {
@@ -169,4 +203,50 @@ public class DashboardActivity extends AppCompatActivity
             }
         });
     }
+
+    private void generateExcelFile(){
+        File file = new File(Environment.getExternalStorageDirectory() + File.separator + "test.xls");
+        try {
+            file.createNewFile();
+
+            WorkbookSettings wbSettings = new WorkbookSettings();
+            wbSettings.setLocale(new Locale("en", "EN"));
+            WritableWorkbook workbook = Workbook.createWorkbook(file, wbSettings);
+
+            workbook.createSheet("Report", 0);
+            WritableSheet excelSheet = workbook.getSheet(0);
+            ExcelUtils.createLabel(excelSheet);
+            ExcelUtils.createContent(excelSheet);
+
+            workbook.write();
+            workbook.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (WriteException ex){
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Checks if the app has permission to write to device storage
+     *
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
 }
---
+++ app/src/main/java/com/keto/optics/utils/ExcelUtils.java
@@ -0,0 +1,98 @@
+package com.keto.optics.utils;
+
+import jxl.CellView;
+import jxl.format.UnderlineStyle;
+import jxl.write.Formula;
+import jxl.write.Label;
+import jxl.write.Number;
+import jxl.write.WritableCellFormat;
+import jxl.write.WritableFont;
+import jxl.write.WritableSheet;
+import jxl.write.WriteException;
+import jxl.write.biff.RowsExceededException;
+
+/**
+ * Created by ketanvichare on 10/9/16.
+ */
+public class ExcelUtils {
+
+    private static WritableCellFormat timesBoldUnderline;
+    private static WritableCellFormat times;
+
+    public static void createLabel(WritableSheet sheet)
+            throws WriteException {
+        // Lets create a times font
+        WritableFont times10pt = new WritableFont(WritableFont.TIMES, 10);
+        // Define the cell format
+        times = new WritableCellFormat(times10pt);
+        // Lets automatically wrap the cells
+        times.setWrap(true);
+
+        // create create a bold font with unterlines
+        WritableFont times10ptBoldUnderline = new WritableFont(
+                WritableFont.TIMES, 10, WritableFont.BOLD, false,
+                UnderlineStyle.SINGLE);
+        timesBoldUnderline = new WritableCellFormat(times10ptBoldUnderline);
+        // Lets automatically wrap the cells
+        timesBoldUnderline.setWrap(true);
+
+        CellView cv = new CellView();
+        cv.setFormat(times);
+        cv.setFormat(timesBoldUnderline);
+        cv.setAutosize(true);
+
+        // Write a few headers
+        addCaption(sheet, 0, 0, "Header 1");
+        addCaption(sheet, 1, 0, "This is second header");
+    }
+
+    private static void addCaption(WritableSheet sheet, int column, int row, String s)
+            throws RowsExceededException, WriteException {
+        Label label;
+        label = new Label(column, row, s, timesBoldUnderline);
+        sheet.addCell(label);
+    }
+
+    public static void createContent(WritableSheet sheet) throws WriteException,
+            RowsExceededException {
+        // Write a few number
+        for (int i = 1; i < 10; i++) {
+            // First column
+            addNumber(sheet, 0, i, i + 10);
+            // Second column
+            addNumber(sheet, 1, i, i * i);
+        }
+        // Lets calculate the sum of it
+        StringBuffer buf = new StringBuffer();
+        buf.append("SUM(A2:A10)");
+        Formula f = new Formula(0, 10, buf.toString());
+        sheet.addCell(f);
+        buf = new StringBuffer();
+        buf.append("SUM(B2:B10)");
+        f = new Formula(1, 10, buf.toString());
+        sheet.addCell(f);
+
+        // now a bit of text
+        for (int i = 12; i < 20; i++) {
+            // First column
+            addLabel(sheet, 0, i, "Boring text " + i);
+            // Second column
+            addLabel(sheet, 1, i, "Another text");
+        }
+
+    }
+
+    private static void addNumber(WritableSheet sheet, int column, int row,
+                           Integer integer) throws WriteException, RowsExceededException {
+        Number number;
+        number = new Number(column, row, integer, times);
+        sheet.addCell(number);
+    }
+
+    private static void addLabel(WritableSheet sheet, int column, int row, String s)
+            throws WriteException, RowsExceededException {
+        Label label;
+        label = new Label(column, row, s, times);
+        sheet.addCell(label);
+    }
+}
